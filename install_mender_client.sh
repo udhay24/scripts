0f038ee8391fe72c4f7efe904361705d82d66db0
udhaya kumar.S <udhay2498@gmail.com>
2025-03-27 16:12:08 +0530

    Update install_mender_client.sh

--- install_mender_client.sh
+++ install_mender_client.sh
@@ -3,8 +3,11 @@
 set -e
 
 # Variables
-MENDER_SERVER_IP="mender.s2tlive.com"
+MENDER_SERVER_IP="43.204.205.12"
 DEVICE_TYPE="codex"
+INVENTORY_POLL=14400  # 4 hours
+RETRY_POLL=600        # 10 minutes
+UPDATE_POLL=3600      # 1 hour
 
 # Update and install dependencies
 echo "Updating package lists and installing dependencies..."
@@ -19,7 +22,10 @@ sudo bash -c "$(curl -fLsS https://get.mender.io)"
 echo "Configuring Mender client..."
 sudo mender setup \
             --device-type $DEVICE_TYPE \
-            --server-ip $MENDER_SERVER_IP
+            --server-ip $MENDER_SERVER_IP \
+            --inventory-poll $INVENTORY_POLL \
+            --retry-poll $RETRY_POLL \
+            --update-poll $UPDATE_POLL
 
 # Restart Mender servicewhen
 echo "Restarting Mender service..."
